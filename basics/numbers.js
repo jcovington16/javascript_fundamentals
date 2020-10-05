659b7e95f49ebaf4e1fa06ec5b1123470d7389f7
jcybertech <jcyber2000@gmail.com>
2020-10-04 21:22:56 -0600

    just going back over the basics. will start bigger projects after reviews.

---
+++ basics/numbers.js
@@ -0,0 +1,23 @@
+let x = 11
+let num = (x+1) * 2
+
+console.log(num)
+
+let age = 26
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+// Challenge area
+
+// studnetScore 18  
+// maxScore 20
+// percent 90
+let studentScore = 18
+let maxScore = 20
+
+let percent = (studentScore / maxScore) * 100
+console.log(percent + '%')
+
+
+// print percent
---
+++ basics/strings.js
@@ -0,0 +1,11 @@
+let first_name = 'Joshua'
+let last_name = 'Covington'
+let full_name = first_name + ' ' + last_name
+
+console.log(full_name)
+
+let city = 'Colorado Springs'
+let country = 'United States'
+let location = city + ', ' + country
+
+console.log(location)
