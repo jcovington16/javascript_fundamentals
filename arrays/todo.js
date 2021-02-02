f6ea80e3870994a7bfd01cc33e20773f2106779f
jcovington16 <covingtonjoshua7@gmail.com>
2021-02-01 22:00:18 -0700

    Working on creating and reading arrays.

---
+++ arrays/todo.js
@@ -0,0 +1,15 @@
+// Create an array with five todos
+// Print You have x todos
+// Print first and second to last items -> Todo: walk the dog
+
+const myTodo = [
+  'Watch school lesson',
+  'Practive JavaScript',
+  'Work on portfolio',
+  'Pick a framework',
+  'Pick an application to build',
+]
+
+console.log(`You have ${myTodo.length} todos.`)
+
+console.log(`Todo: ${myTodo[0]} and ${myTodo[myTodo.length - 2]}`)
